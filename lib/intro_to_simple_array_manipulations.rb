eeb8aef53afdad3f313135595eba1739d785db89
Brandon Brown <brandonbrown4792@gmail.com>
2020-02-25 20:03:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  array.pop(2)
+  array.shift(2)
   return array
 end
 
@@ -20,7 +20,7 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  array.shift(2)
+  array.pop(2)
   return array
 end
 
