2485f87ac9e8280d3d1ccfffbcc2e6ef17b0a9a2
Brandon Brown <brandonbrown4792@gmail.com>
2020-02-25 19:53:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,19 @@ end
 def using_insert(array, element)
   return array.insert(element, 4)
 end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, index)
+  return array.delete(index)
+end
